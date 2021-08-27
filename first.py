6c3acadb82f6718a19c7409cd37fb082cc162c49
Michael Kahen <88952815+muykel@users.noreply.github.com>
2021-08-27 00:48:56 -0400

    Changes

--- first.py
+++ first.py
@@ -1,20 +1,23 @@
 import pygame
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
+# from pygame.locals import (
+#     K_UP,
+#     K_DOWN,
+#     K_LEFT,
+#     K_RIGHT,
+#     K_ESCAPE,
+#     KEYDOWN,
+#     QUIT,
+# )
 pygame.init()
 mainScreen = pygame.display.set_mode([1280, 800])
+pygame.display.set_caption('Gabe is trash :joy:')
+rinPic = pygame.image.load('rinpygame.jpeg')
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    mainScreen.fill((255, 255, 255))
-    pygame.display.flip()
+    #mainScreen.fill((255, 255, 255))
+    mainScreen.blit(rinPic, (300,300))
+    pygame.display.update() 
