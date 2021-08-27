2195cca5fe0b1e1926425b476409521ffcc736c6
Michael Kahen <88952815+muykel@users.noreply.github.com>
2021-08-26 23:08:36 -0400

    Update first.py

--- first.py
+++ first.py
@@ -13,10 +13,8 @@ mainScreen = pygame.display.set_mode([1280, 800])
 
 running = True
 while running:
-    # Code to quit the app
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     mainScreen.fill((255, 255, 255))
-
     pygame.display.flip()
