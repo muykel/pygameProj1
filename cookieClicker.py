4b2d53aea446eed77da34a86d91db395394f598a
Michael Kahen <88952815+muykel@users.noreply.github.com>
2021-08-29 01:35:52 -0400

    Update cookieClicker.py

--- cookieClicker.py
+++ cookieClicker.py
@@ -1,22 +1,16 @@
-import pygame
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
+import pygame, math
+from pygame.locals import *
 
 # figure out how to count seconds in game
 
 pygame.init()
-start_time = None
+# FPS = 15
+start_time = 0
+button_start_time = 0
 clock = pygame.time.Clock()
-mainScreen = pygame.display.set_mode([1280, 750])
+mainScreen = pygame.display.set_mode([300, 300])
 pygame.display.set_caption('Cookie Clicker')
-cookie = pygame.image.load('cookiez.png')
+# cookie = pygame.image.load('cookiez.png')
 cookie2 = pygame.image.load('cookiez2.png')
 
 running = True
@@ -27,3 +21,9 @@ while running:
     mainScreen.fill((0, 100, 255))
     mainScreen.blit(cookie2, (640,375))
     pygame.display.update() 
+    #if(start_time%1000 == 0):
+    #start_time = pygame.time.get_ticks()
+    #print(math.trunc(start_time/1000)) # 1 tick = 1 millisecond
+    #print(start_time/1000)
+    if(not float(start_time/1000).is_integer()):
+        print(start_time)
