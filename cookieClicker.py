f68d89721dd31f250f890daed6776c607a963adf
Michael Kahen <88952815+muykel@users.noreply.github.com>
2021-08-29 01:55:49 -0400

    Update cookieClicker.py

--- cookieClicker.py
+++ cookieClicker.py
@@ -5,13 +5,13 @@ from pygame.locals import *
 
 pygame.init()
 # FPS = 15
-start_time = 0
+start_time = pygame.time.get_ticks() # first tick
 button_start_time = 0
 clock = pygame.time.Clock()
 mainScreen = pygame.display.set_mode([300, 300])
 pygame.display.set_caption('Cookie Clicker')
 # cookie = pygame.image.load('cookiez.png')
-cookie2 = pygame.image.load('cookiez2.png')
+# cookie2 = pygame.image.load('cookiez2.png')
 
 running = True
 while running:
@@ -19,11 +19,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     mainScreen.fill((0, 100, 255))
-    mainScreen.blit(cookie2, (640,375))
+    # mainScreen.blit(cookie2, (640,375))
     pygame.display.update() 
     #if(start_time%1000 == 0):
-    #start_time = pygame.time.get_ticks()
+    start_time = pygame.time.get_ticks()
     #print(math.trunc(start_time/1000)) # 1 tick = 1 millisecond
     #print(start_time/1000)
-    if(not float(start_time/1000).is_integer()):
-        print(start_time)
+    #print(start_time)
+    for i in range(10):
+        pygame.time.wait(1000)
+        print(i)
