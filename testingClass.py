935fe37b815c3dd89ad7e4e9ab6ccaf9938d939c
Michael Kahen <88952815+muykel@users.noreply.github.com>
2021-08-30 12:55:59 -0400

    Added icon, and removed some random stuff

--- testingClass.py
+++ testingClass.py
@@ -1,15 +1,15 @@
 import pygame, math
 from pygame.locals import *
 
-# figure out how to count seconds in game
-
 pygame.init()
 # FPS = 15
 start_time = pygame.time.get_ticks() # first tick
 button_start_time = 0
 clock = pygame.time.Clock()
 mainScreen = pygame.display.set_mode([300, 300])
-pygame.display.set_caption('Cookie Clicker')
+pygame.display.set_caption('Testing Window')
+a = pygame.image.load('dragonicon.png')
+pygame.display.set_icon(a)
 # cookie = pygame.image.load('cookiez2.png')
 
 running = True
@@ -20,11 +20,7 @@ while running:
     mainScreen.fill((0, 100, 255))
     # mainScreen.blit(cookie2, (640,375))
     pygame.display.update() 
-    #if(start_time%1000 == 0):
-    start_time = pygame.time.get_ticks()
-    #print(math.trunc(start_time/1000)) # 1 tick = 1 millisecond
-    #print(start_time/1000)
-    #print(start_time)
-    for i in range(10):
-        pygame.time.wait(1000)
-        print(i)
+    # start_time = pygame.time.get_ticks()
+    # for i in range(10):
+    #     pygame.time.wait(1000)
+    #     print(i)
