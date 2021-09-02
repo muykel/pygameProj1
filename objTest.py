584af3f2a7b902802255f9bb61e5f5a5c2b0df43
Michael Kahen <88952815+muykel@users.noreply.github.com>
2021-09-02 15:57:03 -0400

    Update objTest.py

--- objTest.py
+++ objTest.py
@@ -4,8 +4,8 @@ class spriteInfo:
     def __init__(self, xcoord, ycoord):
         self.xcoord = xcoord
         self.ycoord = ycoord
-    def getx(self):
+    def get_x(self):
         return self.xcoord
         
 yes = spriteInfo(50,100)
-print(yes.getx)
+print(yes.get_x)
